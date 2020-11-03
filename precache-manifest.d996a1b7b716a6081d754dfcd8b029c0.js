4600fadc8d9c816467aa4807993505ef93514734
pos <possesion193@gmail.com>
2020-11-03 13:26:19 +0300

    Updates

---
+++ precache-manifest.d996a1b7b716a6081d754dfcd8b029c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cf54e1c70b0a125b4d30b670c63f8a",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "44684e246a3fab06b62f",
+    "url": "/shopping-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "865dfb60cafa676d2ce7",
+    "url": "/shopping-app/static/css/main.19a8ce8d.chunk.css"
+  },
+  {
+    "revision": "44684e246a3fab06b62f",
+    "url": "/shopping-app/static/js/2.5c410fc0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/shopping-app/static/js/2.5c410fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865dfb60cafa676d2ce7",
+    "url": "/shopping-app/static/js/main.7c6386d8.chunk.js"
+  },
+  {
+    "revision": "a4290364a931f3240049",
+    "url": "/shopping-app/static/js/runtime-main.3e77fe51.js"
+  }
+]);
