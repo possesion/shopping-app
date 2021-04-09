4f3422d3813cad5c9854483a1c1372718d26bfe4
Gennadiy K <possesion193@gmail.com>
2021-04-09 17:35:48 +0300

    add test for todoList

--- src/features/todos/Todo.test.js
+++ src/features/todos/Todo.test.js
@@ -1,43 +1,71 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { render, cleanup, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Todo from './Todo'
-import { store } from '../../components/App'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import Todo from './Todo';
+import { store } from '../../components/App';
 
-afterEach(cleanup);
+beforeEach(cleanup);
 
-const vdom = () => render(<Provider store={store}><Todo/></Provider>);
+jest.mock('react-transition-group', () => {
+  const FakeTransition = jest.fn(({ children }) => children);
+  const FakeCSSTransition = jest.fn((props) => (
+    <FakeTransition>{props.children}</FakeTransition>
+  ));
+  return { CSSTransition: FakeCSSTransition, TransitionGroup: FakeTransition };
+});
+
+const vdom = () =>
+  render(
+    <Provider store={store}>
+      <Todo />
+    </Provider>
+  );
 
 it('render Todo component', () => {
-    render(<Provider store={store}><Todo/></Provider>)
+  render(
+    <Provider store={store}>
+      <Todo />
+    </Provider>
+  );
 });
 
 it('add new Task', () => {
-   const { getByTestId, getByText } = vdom();
-   fireEvent.change(getByTestId('input'), { target: { value: 'firstTask' } })
-   fireEvent.click(getByText('add to list'))
-   expect(getByTestId('element').textContent).toBe('firstTask')
-})
+  const { getByTestId, getByText } = vdom();
+  fireEvent.change(getByTestId('input'), { target: { value: 'firstTask' } });
+  fireEvent.click(getByText('add to list'));
+  // screen.debug();
+  expect(getByTestId('element').textContent).toBe('firstTask');
+});
+it('change filter', () => {
+  const { queryByTestId, getByText } = vdom();
+  expect(queryByTestId('element')).toBeInTheDocument();
+  userEvent.click(getByText(/Done/gi));
+  expect(queryByTestId('element')).not.toBeInTheDocument();
+});
 
 it('add second Task', () => {
-    const { getByTestId, getByText, getAllByTestId } = vdom()
-    fireEvent.change(getByTestId('input'), { target: { value: 'anotherTask' } })
-    fireEvent.click(getByText('add to list'))
-    expect(getAllByTestId('elements').length).toBe(2)
- })
+  const { getByTestId, getByText, getAllByTestId } = vdom();
+  fireEvent.click(getByText(/All/gi));
+  fireEvent.change(getByTestId('input'), { target: { value: 'anotherTask' } });
+  fireEvent.click(getByText('add to list'));
+  expect(getAllByTestId('listEl').length).toBe(2);
+});
 
 it('remove task', () => {
-    const { getAllByText, getAllByTestId } = vdom()
-    const task = getAllByText('×');
-    fireEvent.click(task[0])
-    expect(getAllByTestId('elements').length).toBe(1)
-})
+  const { queryAllByTestId } = vdom();
+  const task = queryAllByTestId('close');
+  expect(queryAllByTestId('listEl').length).toBe(2);
+  fireEvent.click(task[0]);
+  expect(queryAllByTestId('listEl').length).toBe(1);
+});
 
 it('switch state to finished', () => {
-    const { getByTestId} = vdom();
-    const task = getByTestId('element')
-    fireEvent.click(task)
-    expect(task.style.textDecoration).toBe('line-through')
-})
+  const { getByTestId } = vdom();
+  const task = getByTestId('listEl');
+  expect(task).toHaveClass('alert-warning');
+  userEvent.click(task);
+  // screen.debug();
+  expect(task).toHaveClass('alert-warning');
+});
--- src/features/todos/TodoList.jsx
+++ src/features/todos/TodoList.jsx
@@ -1,73 +1,74 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTask, deleteTask } from './todosSlice';
-import {
-    CSSTransition,
-    TransitionGroup,
-} from 'react-transition-group';
-import _ from 'lodash'
-import '../../assets/todo-list.scss'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import _ from 'lodash';
+import '../../assets/todo-list.scss';
 
 const TodoList = () => {
-    const dispatch = useDispatch();
-    const byId = useSelector(state => state.tasks.byId);
-    const allIds = useSelector(state => state.tasks.allIds);
-    const currentFilter = useSelector(state => state.tasks.currentNameFilter);
-    const listRef = useRef();
+  const dispatch = useDispatch();
+  const byId = useSelector((state) => state.tasks.byId);
+  const allIds = useSelector((state) => state.tasks.allIds);
+  const currentFilter = useSelector((state) => state.tasks.currentNameFilter);
+  const listRef = useRef();
 
+  const isDone = (taskState) => {
+    const defaultClasses = [
+      'd-flex', 'justify-content-between', 'my-2' , 'list-element'
+    ];
+   return taskState === 'finished'
+      ? `alert-success ${defaultClasses.join(' ')}`
+      : `alert-warning ${defaultClasses.join(' ')}`;
+  };
+  const handleToggle = (id) => {
+    // e.preventDefault();
+    if (id) dispatch(toggleTask(id));
+  };
+  const handleDelete = (id) => () => {
+    dispatch(deleteTask(id));
+  };
+  const filtered =
+    currentFilter === 'all'
+      ? allIds
+      : allIds.filter((i) => {
+          return byId[i].state === currentFilter;
+        });
 
-    const handleToggle = (id) => (e) => {
-        e.preventDefault();
-        dispatch(toggleTask(id));
-    };
-    const handleDelete = (id) => () => {
-        dispatch(deleteTask(id));
-    };
-    const filtered = currentFilter === 'all' ? allIds : allIds.filter(i => {
-        return byId[i].state === currentFilter;
-    });
-
-    return (
-        <div className="mt-3">
-            <ul ref={listRef} className="list-group todo-list">
-                <TransitionGroup className="todo-list">
-                    {filtered.map((idx) => {
-                        const { id, text, state } = byId[idx];
-                        return (
-                            <CSSTransition key={id}
-                                timeout={500}
-                                classNames="item"
-                            >
-                                <React.Fragment key={_.uniqueId(id)}>
-                                    <li className="list-element d-flex" data-testid="elements">
-                                        <span className="mr-auto">
-                                            <button className="btn btn-link"
-                                                data-testid="element"
-                                                onClick={handleToggle({ id })}
-                                                style={{
-                                                    textDecoration: state === 'finished' ?
-                                                        'line-through' :
-                                                        'none'
-                                                }}>
-                                                <div className="text">
-                                                    {text}
-                                                </div>
-                                            </button>
-                                        </span>
-                                        <button className="close"
-                                            onClick={handleDelete({ id })}>
-                                            <span>×</span>
-                                        </button>
-                                    </li>
-                                </React.Fragment>
-                            </CSSTransition>
-                        )
-                    })}
-
-                </TransitionGroup>
-            </ul>
-        </div>
-    )
+  return (
+    <div className='mt-3'>
+      {/* <ul ref={listRef} className='list-group todo-list'> */}
+        <TransitionGroup className='todo-list'>
+          {filtered &&
+            filtered.map((idx) => {
+              const { id, text, state } = byId[idx];
+              return (
+                <CSSTransition key={id} timeout={500} classNames='item'>
+                  <React.Fragment key={_.uniqueId(id)}>
+                    <div className={isDone(state)} data-testid='listEl'>
+                        <button
+                          className='btn btn-link w-100'
+                          type="button"
+                          data-testid='element'
+                          onClick={() => handleToggle({ id })}
+                        >
+                          <div className='text'>{text}</div>
+                        </button>
+                      <button
+                        className='close'
+                        data-testid='close'
+                        onClick={handleDelete({ id })}
+                      >
+                        <span>×</span>
+                      </button>
+                    </div>
+                  </React.Fragment>
+                </CSSTransition>
+              );
+            })}
+        </TransitionGroup>
+      {/* </ul> */}
+    </div>
+  );
 };
 
 export default TodoList;
